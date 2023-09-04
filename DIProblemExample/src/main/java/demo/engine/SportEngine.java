f273bb8406277e3b08deb9523f393621b4e709d7
Haaris Sayyed <haaris7200@gmail.com>
2023-09-04 18:47:43 +0530

    Added example to demonstrate use of @Qualifier and @Primary annotations

---
+++ DIProblemExample/src/main/java/demo/engine/SportEngine.java
@@ -0,0 +1,17 @@
+package demo.engine;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class SportEngine implements Engine{
+	
+	private final int hp = 200;
+    @Override
+	public int getHp() {
+		return hp;
+	}
+	
+
+}
