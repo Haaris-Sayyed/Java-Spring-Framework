e80a2bf163d53153f68ede766797347b23b75723
Haaris Sayyed <haaris7200@gmail.com>
2023-09-04 18:37:03 +0530

    Added example for dependency injection

---
+++ DIExample/src/main/java/demo/Car.java
@@ -0,0 +1,29 @@
+package demo;
+
+public class Car {
+	
+	private Engine engine;
+
+//	Without DI
+//	public Car() {
+//		engine = new Engine();
+//	}
+	
+	public Car() {
+		
+	}
+	
+//	With DI
+	public Car(Engine engine) {
+		this.engine = engine;
+	}
+
+	public void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+	
+	public String showEngineHP() {
+		return "hp: " + engine.getHp();
+	}
+
+}
---
+++ DIExample/src/main/java/demo/Engine.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Engine {
+	
+	private final int hp = 200;
+
+	public int getHp() {
+		return hp;
+	}
+	
+
+}
