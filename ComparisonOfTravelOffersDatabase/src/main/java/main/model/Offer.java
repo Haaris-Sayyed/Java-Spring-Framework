60b9333e0fad3e0b829620a5db5327b87e84a4e0
Haaris Sayyed <haaris7200@gmail.com>
2023-09-05 16:12:38 +0530

    Added configuration for spring boot hibernate

---
+++ ComparisonOfTravelOffersDatabase/src/main/java/main/model/Offer.java
@@ -0,0 +1,53 @@
+package main.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "offer")
+public class Offer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String code;
+	private String name;
+	private long duration;
+	private Date creationDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getDuration() {
+		return duration;
+	}
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+}
---
+++ ComparisonOfTravelOffersDatabase/src/main/java/main/service/OfferServiceImpl.java
@@ -0,0 +1,49 @@
+package main.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import main.model.Offer;
+import main.model.OfferDTO;
+import main.repository.OfferRepository;
+
+public class OfferServiceImpl implements OfferService{
+	
+	@Autowired
+	private OfferRepository offerRepository;
+
+	@Override
+	public List<Offer> getAll() {
+		return offerRepository.findAll();
+	}
+
+	@Override
+	public Offer getById(int id) {
+		return offerRepository.getOne(id);
+	}
+
+	@Override
+	public void saveOrUpdate(Offer offer) {
+		if(offer.getId() == 0) {
+			offer.setCreationDate(new Date());
+		}
+		offerRepository.save(offer);
+	}
+
+	@Override
+	public void delete(int id) {
+		offerRepository.deleteById(id);		
+	}
+
+	@Override
+	public List<OfferDTO> getAllDTO() {
+		List<Offer> offers = getAll();
+		List<OfferDTO> offersDTO = offers.stream().map(o -> new OfferDTO(o.getId(), o.getCode(), o.getName(), o.getDuration(), false, "Local Offer"))
+				.collect(Collectors.toList());
+		return offersDTO;
+	}
+
+}
