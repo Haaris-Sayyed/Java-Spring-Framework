4dfbe3f48a7941e83a4cb1cd0395fcf1e98d02e5
Haaris Sayyed <haaris7200@gmail.com>
2023-09-05 10:52:05 +0530

    Added example to demonstrate one to one mapping

---
+++ TravelAgencyOneToOne/src/main/service/TourService.java
@@ -0,0 +1,16 @@
+package main.service;
+import java.util.List;
+import main.model.Tour;
+
+public interface TourService {
+	
+	public List<Tour> getAll();	
+	
+	public Tour getById(int id);
+	
+	public void saveOrUpadate(Tour tour);
+	
+	public void delete(int id);
+	
+	public void addTourDetailsIfNotExists(Tour tour);
+}
