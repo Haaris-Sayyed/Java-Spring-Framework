88370f12762cb49937495af42f62b7fbbe7e589c
Haaris Sayyed <haaris7200@gmail.com>
2023-09-05 15:15:17 +0530

    Created different api endpoints to perform crud operations on tours

---
+++ TravelAgencyRestTourCRUD/src/main/restcontroller/FirstRestController.java
@@ -0,0 +1,13 @@
+package main.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstRestController {
+
+	@GetMapping("/restexample")
+	public String hello() {
+		return "Hello from Rest API";
+	}
+}
